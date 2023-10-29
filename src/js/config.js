9f30e3f8d7b4554faf9fd3a6426317b10aa1d8e2
kevintran614 <kevinat1@uci.edu>
2023-10-29 12:44:07 -0700

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '7b7e18a1-d627-4a9c-9510-b9ac5d48aa05';
+export const MODAL_CLOSE_SEC = 2.5;
